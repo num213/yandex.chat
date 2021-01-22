5124bd288abb23b3434925b9a6f1c5c19e552bf7
Andrew Zolotov <num213@gmail.com>
2021-01-22 21:44:03 +0300

    dockerizing node app

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # yandex.chat
+
+## npm
+npm run start
+
+## Docker
+* docker build -t num213/yandex-chat .
+* docker run -e HOST="0.0.0.0" -p 49160:8080 -d --name yandex_chat num213/yandex-chat
---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = process.env.HOST || '127.0.0.1';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+	res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
+console.log('process.env', process.env);
+console.log(`Running on http://${HOST}:${PORT}`);
