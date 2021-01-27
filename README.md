3ad8c8441ee69122f0b563e8c09dce448acb410c
Andrew Zolotov <num213@gmail.com>
2021-01-27 23:15:01 +0300

    Update paths for netlify

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# yandex.chat
+# Чат для проектной работы в Яндекс.Практикуме
 
-## Run
+## Run, forest, run
 
 ### npm
 npm run build
@@ -9,6 +9,7 @@ npm run build
 * docker build -t num213/yandex-chat .
 * docker run -e HOST="0.0.0.0" -p 49160:8080 -d --name yandex_chat num213/yandex-chat
 
-## Documentation
+## Demo
 
-[Figma](https://www.figma.com/file/1zDWXw7uH1yC4A4RnC7HhW/Yandex.Praktikum.Chat?node-id=0%3A1)
+* [Figma](https://www.figma.com/file/1zDWXw7uH1yC4A4RnC7HhW/Yandex.Praktikum.Chat?node-id=0%3A1)
+* [Netlify](https://dreamy-murdock-f2e16a.netlify.app/)
--- server.js
+++ server.js
@@ -7,34 +7,30 @@ const app = express();
 const PORT = 8080;
 const HOST = process.env.HOST || '127.0.0.1';
 
+app.use(express.static(path.resolve(__dirname, 'static')));
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/static/index.html'));
-});
-
-app.get('/readme', function(req, res) {
-	res.sendFile(path.join(__dirname + '/README.md'));
+	res.sendFile(path.resolve(__dirname, 'static', 'index.html'));
 });
 app.get('/login', function(req, res) {
-	res.sendFile(path.join(__dirname + '/static/login.html'));
+	res.sendFile(path.resolve(__dirname, 'static', 'login.html'));
 });
 app.get('/register', function(req, res) {
-	res.sendFile(path.join(__dirname + '/static/register.html'));
+	res.sendFile(path.resolve(__dirname, 'static', 'register.html'));
 });
 app.get('/chats', function(req, res) {
-	res.sendFile(path.join(__dirname + '/static/chats.html'));
+	res.sendFile(path.resolve(__dirname, 'static', 'chats.html'));
 });
 app.get('/chats-select', function(req, res) {
-	res.sendFile(path.join(__dirname + '/static/chats-no-selected.html'));
+	res.sendFile(path.resolve(__dirname, 'static', 'chats-select.html'));
 });
 app.get('/profile', function(req, res) {
-	res.sendFile(path.join(__dirname + '/static/profile.html'));
+	res.sendFile(path.resolve(__dirname, 'static', 'profile.html'));
 });
-app.get('/profile/update', function(req, res) {
-	res.sendFile(path.join(__dirname + '/static/profile-update.html'));
+app.get('/profile-update', function(req, res) {
+	res.sendFile(path.resolve(__dirname, 'static', 'profile-update.html'));
 });
-app.get('/profile/password', function(req, res) {
-	res.sendFile(path.join(__dirname + '/static/profile-password.html'));
+app.get('/profile-password', function(req, res) {
+	res.sendFile(path.resolve(__dirname, 'static', 'profile-password.html'));
 });
 
-app.use(express.static(__dirname + '/static/'));
 app.listen(PORT, HOST);
